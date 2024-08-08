6f1232b29f60c9d1b965a696222171e73476e01b
Jagdish Patel <jagdishpatelsoft@gmail.com>
2024-08-08 18:31:23 +0530

    chng

--- application/views/party/crm_desk.php
+++ application/views/party/crm_desk.php
@@ -45,7 +45,7 @@
                                         </div>
                                         <div>
                                             <a href="javascript:void(0);" class="dropdown-toggle btn gradient-theme d-block d-flex justify-content-between" id="leadShortDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                Short By <span><?=getIcon('sliders')?></span>
+                                                Short By<br><small>Name</small> <span><?=getIcon('sliders')?></span>
                                             </a>
                                             <div class="dropdown-menu position-absolute" aria-labelledby="leadShortDropdown">
                                                 <div class="dropdown-item">
