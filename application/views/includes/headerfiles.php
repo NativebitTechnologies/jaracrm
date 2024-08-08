8c327f12c9af7291d99128a68b103cc48b044e54
Jagdish Patel <jagdishpatelsoft@gmail.com>
2024-08-08 11:08:25 +0530

    remove search css from headerfiles

--- application/views/includes/headerfiles.php
+++ application/views/includes/headerfiles.php
@@ -23,6 +23,6 @@
     <!--  BEGIN CUSTOM STYLE FILE  -->
     <link href="<?=base_url();?>assets/src/dt_table.css" rel="stylesheet" type="text/css" />
     <link href="<?=base_url();?>assets/src/plugins/css/light/table/datatable/dt-global_style.css" rel="stylesheet" type="text/css">
-    <link href="<?=base_url();?>assets/src/assets/css/light/elements/search.css" rel="stylesheet" type="text/css" />
+     <!-- <link href="<?=base_url();?>assets/src/assets/css/light/elements/search.css" rel="stylesheet" type="text/css" />-->
     <!--  END CUSTOM STYLE FILE  -->
 </head>
