7194761c1df442a5f951fa2f7c16ff79b9c9ae90
Jagdish Patel <jagdishpatelsoft@gmail.com>
2024-08-08 17:29:43 +0530

    add btn

--- application/views/party/crm_desk.php
+++ application/views/party/crm_desk.php
@@ -20,10 +20,11 @@
                                 <div class="tab-title bg-aliceblue">
                                     <div class="row">
                                         <div class="col-md-12 col-sm-12 col-12 text-center">
+                                            <a class="btn btn-secondary" id="addTask" href="#"><?=getIcon('plus')?> New Lead</a>
                                             <h5 class="app-title">CRM DESK</h5>
                                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                                                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
-                                            </button>
+                                                          <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                                                        </button>
                                         </div>
                                         <div class="col-md-12 col-sm-12 col-12 ps-0 pe-0">
                                             <div class="todoList-sidebar-scroll mt-1">
