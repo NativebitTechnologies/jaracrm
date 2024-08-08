5747c5d33a7bab52bf0fc4ca92cc83164aca4b4d
Jagdish Patel <jagdishpatelsoft@gmail.com>
2024-08-08 18:27:04 +0530

    update crm desk css & view

--- application/views/includes/headerfiles.php
+++ application/views/includes/headerfiles.php
@@ -5,15 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
     <link rel="icon" type="image/x-icon" href="<?=base_url();?>assets/src/assets/img/favicon.ico"/>
     <link href="<?=base_url();?>assets/layouts/collapsible-menu/css/light/loader.css" rel="stylesheet" type="text/css" />
-    <link href="<?=base_url();?>assets/layouts/collapsible-menu/css/dark/loader.css" rel="stylesheet" type="text/css" />
     <script src="<?=base_url();?>assets/layouts/collapsible-menu/loader.js"></script>
 
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
     <link href="<?=base_url();?>assets/src/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link href="<?=base_url();?>assets/layouts/collapsible-menu/css/light/plugins.css" rel="stylesheet" type="text/css" />
-    <!--<link href="<?=base_url();?>assets/layouts/collapsible-menu/css/dark/plugins.css" rel="stylesheet" type="text/css" />-->
-    <link href="<?=base_url();?>assets/src/plugins/src/font-icons/fontawesome/css/fontawesome.css" rel="stylesheet" type="text/css" />
     <link href="<?=base_url();?>assets/src/jp_styles.css" rel="stylesheet" type="text/css" />
     <link href="<?=base_url();?>assets/src/assets/css/light/components/modal.css" rel="stylesheet" type="text/css" />
     <!-- END GLOBAL MANDATORY STYLES -->
@@ -23,6 +20,5 @@
     <!--  BEGIN CUSTOM STYLE FILE  -->
     <link href="<?=base_url();?>assets/src/dt_table.css" rel="stylesheet" type="text/css" />
     <link href="<?=base_url();?>assets/src/plugins/css/light/table/datatable/dt-global_style.css" rel="stylesheet" type="text/css">
-     <!-- <link href="<?=base_url();?>assets/src/assets/css/light/elements/search.css" rel="stylesheet" type="text/css" />-->
     <!--  END CUSTOM STYLE FILE  -->
 </head>
