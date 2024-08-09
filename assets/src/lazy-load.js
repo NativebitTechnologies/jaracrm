5f1842b8596c7bf731f5451a8a67f5cc5c328c42
Jagdish Patel <jagdishpatelsoft@gmail.com>
2024-08-09 13:15:53 +0530

    lazy load

--- assets/src/lazy-load.js
+++ assets/src/lazy-load.js
@@ -85,7 +85,7 @@ function loadTransaction(){
     var length = $(".lazy-load-trans").data('length') || 20;
     var filter_page_name = $(".lazy-load-trans").data('filter_page_name') || "";
     var postData = $(".lazy-load-trans").data('post_data') || {};
-
+console.log(postData);
     var filterData = {};
     if(filter_page_name){
         var flData = localStorage.getItem(filter_page_name);
