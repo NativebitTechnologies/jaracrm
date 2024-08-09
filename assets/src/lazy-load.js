ef051652311b7f2a17cee69fb9c7d0aa470f1d53
Jagdish Patel <jagdishpatelsoft@gmail.com>
2024-08-09 15:36:45 +0530

    sdfds

--- assets/src/lazy-load.js
+++ assets/src/lazy-load.js
@@ -138,13 +138,13 @@ function reloadTransaction(){
 
 function tabLoading(tabId){
 	load_flag = 0;ajax_call = false;
-	$(".lazy-load-trans").removeAttr('data-url');
+	//$(".lazy-load-trans").removeAttr('data-url');
 	$(".lazy-load-trans").attr('data-url',$("#"+tabId).data('url'));
 
     $(".lazy-load-trans").removeAttr('data-post_data');
     $(".lazy-load-trans").data('post_data',($("#"+tabId).data('post_data') || "{}"));
 
-    $(".lazy-load-trans").removeAttr('data-length');
+    //$(".lazy-load-trans").removeAttr('data-length');
     $(".lazy-load-trans").data('length',($("#"+tabId).data('length') || 20));
 
     $(".lazy-load-trans").html('');
