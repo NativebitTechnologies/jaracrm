771fdc44e94f06c814e2ad27050fe747e5f8c6aa
Jagdish Patel <jagdishpatelsoft@gmail.com>
2024-08-10 12:26:44 +0530

    asd

--- assets/src/comman-js.js
+++ assets/src/comman-js.js
@@ -397,6 +397,7 @@ function store(postData){
 	}).done(function(data){
 		if(data.status==1){
 			$('#'+formId)[0].reset(); closeModal(formId);
+			console.log(data.responseEle + ' = ' + data.responseHtml);
 			if(data.responseHtml != ""){
 				$(data.responseEle).html(data.responseHtml);
 			}
