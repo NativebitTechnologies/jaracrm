f39439bca01c1afaadd45e3f5f71de8e83035714
Jagdish Patel <jagdishpatelsoft@gmail.com>
2024-08-10 16:12:48 +0530

    bug

--- application/views/configuration/stage_form.php
+++ application/views/configuration/stage_form.php
@@ -10,7 +10,7 @@
 			<?php }else{ ?>
                 <div class="col-md-12 form-group">
                     <h5 class="text-danger font-bold fs-14"> Your Maximum Stage Limit is : <?=$MAX_LEAD_STAGE?></h5>
-                    <label for="stage_type">Stage Name <?=$next_seq_no?></label>
+                    <label for="stage_type">Stage Name</label>
                     <input type="text" name="stage_type" id="stage_type" class="form-control req" value="<?=(!empty($dataRow->stage_type) ? $dataRow->stage_type : "")?>" />
                 </div>
             
