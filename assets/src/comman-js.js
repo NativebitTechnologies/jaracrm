8eeaff6b54c25ca5d67a3fc7c33c8499be26ae82
Jagdish Patel <jagdishpatelsoft@gmail.com>
2024-08-10 12:22:59 +0530

    response changes

--- assets/src/comman-js.js
+++ assets/src/comman-js.js
@@ -398,7 +398,7 @@ function store(postData){
 		if(data.status==1){
 			$('#'+formId)[0].reset(); closeModal(formId);
 			if(data.responseHtml != ""){
-				$("."+data.responseClass).html(data.responseHtml);
+				$(data.responseEle).html(data.responseHtml);
 			}
 			//Swal.fire({ icon: 'success', title: data.message});
 			//Toast.fire({icon: 'success',title: data.message});
