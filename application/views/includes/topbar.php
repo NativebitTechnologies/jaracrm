2b034e45c64f8b662d07fe188b3612ed7acf0848
Jagdish Patel <jagdishpatelsoft@gmail.com>
2024-08-16 18:41:52 +0530

    f

--- application/views/includes/topbar.php
+++ application/views/includes/topbar.php
@@ -1,12 +1,12 @@
 <!--  BEGIN NAVBAR  -->
 <div class="header-container">
 	<header class="header navbar navbar-expand-sm expand-header">
-		<h5 class="page_title"><?=(!empty($headData->pageTitle)) ? $headData->pageTitle : SITENAME?></h5>
 		<a href="javascript:void(0);" class="sidebarCollapse">
 			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
 			<!--<h5 class="page_title"><?=(!empty($headData->pageTitle)) ? $headData->pageTitle : SITENAME?></h5>-->
 		</a>
-
+		<h5 class="page_title"><?=(!empty($headData->pageTitle)) ? $headData->pageTitle : SITENAME?></h5>
+		
 		<ul class="navbar-item flex-row ms-lg-auto ms-0">
 			
 			<li class="nav-item dropdown notification-dropdown">
