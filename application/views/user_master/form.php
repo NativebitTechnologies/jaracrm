ba1d2207c98f7a3cbd9fb5dceacd7f8989f23a39
Nayan Chikhaliya <nayan.chikhaliya@gmail.com>
2024-08-18 11:20:37 +0530

    User Form change

--- application/views/user_master/form.php
+++ application/views/user_master/form.php
@@ -2,32 +2,20 @@
     <div class="col-md-12">
         <div class="row">
             <input type="hidden" name="id" id="id" value="<?=(!empty($dataRow->id))?$dataRow->id:""?>">
+            <input type="hidden" name="user_role" id="user_role" value="<?=(!empty($dataRow->user_role))?$dataRow->user_role:1?>">
 
-            <div class="col-md-4 form-group">
-                <label for="user_code">User Code</label>
-                <input type="text" name="user_code" id="user_code" class="form-control req" value="<?=(!empty($dataRow->user_code))?$dataRow->user_code:""?>">
-            </div>
-
-            <div class="col-md-8 form-group">
+            <div class="col-md-12 form-group">
                 <label for="user_name">User Name</label>
                 <input type="text" name="user_name" id="user_name" class="form-control req" value="<?=(!empty($dataRow->user_name))?$dataRow->user_name:""?>">
             </div>
-
-            <div class="col-md-6 form-group">
-                <label for="user_role">Role</label>
-                <select name="user_role" id="user_role" class="form-control selectBox req">
-                    <option value="">Select Role</option>
-                    <?php
-                        foreach($this->empRole as $key=>$roleName):
-                            $selected = (!empty($dataRow->user_role) && $dataRow->user_role == $key)?"selected":"";
-                            echo '<option value="'.$key.'" '.$selected.'>'.$roleName.'</option>';
-                        endforeach;
-                    ?>
-                </select>
+			
+			<div class="col-md-6 form-group">
+                <label for="user_code">User Code</label><small>(Login ID)</small>
+                <input type="text" name="user_code" id="user_code" class="form-control req" value="<?=(!empty($dataRow->user_code))?$dataRow->user_code:""?>">
             </div>
 
             <div class="col-md-6 form-group">
-                <label for="contact_no">Mobile No.</label>
+                <label for="contact_no">Mobile No.</label><small>(Login ID)</small>
                 <input type="text" name="contact_no" id="contact_no" class="form-control req numericOnly" value="<?=(!empty($dataRow->contact_no))?$dataRow->contact_no:""?>">
             </div>
 
