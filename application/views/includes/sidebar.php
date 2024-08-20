6e3b57fff749e8a5550f0022c2bd11fa03de5f93
Jagdish Patel <jagdishpatelsoft@gmail.com>
2024-08-20 09:52:53 +0530

    dsf

--- application/views/includes/sidebar.php
+++ application/views/includes/sidebar.php
@@ -4,17 +4,17 @@
 		<div class="navbar-nav theme-brand flex-row  text-center">
 			<div class="nav-logo">
 				<div class="nav-item theme-logo">
-					<a href="./index.html">
-						<img src="<?=base_url();?>assets/src/assets/img/logo.svg" class="navbar-logo" alt="logo">
+					<a href="<?=base_url('dashboard')?>">
+						<img src="<?=base_url();?>assets/images/icon.png" class="navbar-logo" alt="logo">
 					</a>
 				</div>
 				<div class="nav-item theme-text">
-					<a href="./index.html" class="nav-link"> CORK </a>
+					<a href="<?=base_url('dashboard')?>" class="nav-link"> <span class="page_title"><?=SITENAME?></span> </a>
 				</div>
 			</div>
 			<div class="nav-item sidebar-toggle">
 				<div class="btn-toggle sidebarCollapse">
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevrons-left"><polyline points="11 17 6 12 11 7"></polyline><polyline points="18 17 13 12 18 7"></polyline></svg>
+					<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevrons-left"><polyline points="11 17 6 12 11 7"></polyline><polyline points="18 17 13 12 18 7"></polyline></svg>
 				</div>
 			</div>
 		</div>
