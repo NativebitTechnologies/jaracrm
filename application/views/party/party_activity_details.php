9ac6147a54612da82d7b6c1f466cd3331781fd63
Jagdish Patel <jagdishpatelsoft@gmail.com>
2024-08-22 17:39:50 +0530

    f

--- application/views/party/party_activity_details.php
+++ application/views/party/party_activity_details.php
@@ -21,7 +21,7 @@
                                     </div>
                                     <div class="tb-section-2">
                                         <a class="" href="javascript:void(0);" >'.getIcon('edit').'</a>
-                                        <a class="" href="javascript:void(0);" >'.getIcon('delete').'</a>
+                                        <a class="danger" href="javascript:void(0);" >'.getIcon('delete').'</a>
                                     </div>
                                 </div>';
 
