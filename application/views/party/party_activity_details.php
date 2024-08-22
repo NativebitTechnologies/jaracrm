ad6bea53669ae1bd5aed5fc22fde705911303fcc
Jagdish Patel <jagdishpatelsoft@gmail.com>
2024-08-22 18:38:07 +0530

    ds

--- application/views/party/party_activity_details.php
+++ application/views/party/party_activity_details.php
@@ -20,7 +20,7 @@
                 if($row->lead_stage == 2){$btns = '<a class="" href="javascript:void(0);" >'.getIcon('edit').'</a><a class="danger" href="javascript:void(0);" >'.getIcon('delete','','danger-svg').'</a>';}
                 if($row->lead_stage >= 13){$icon = 'disc';$iconColor = 'bg-polo-blue';}else{$icon = $this->iconClass[$row->lead_stage];$iconColor = $this->iconColor[$row->lead_stage];}
                 
-                echo '<div class="timeline-line">
+                echo '<div class="timeline-line party_activity">
                             <div class="item-timeline timeline-new">
                                 <div class="t-dot">
                                     <div class="'.$iconColor.' white">'.getIcon($icon).'</div>
@@ -43,3 +43,8 @@
         ?>
     </div>
 </div>
+<script>
+$('.party_activity').each((index, element) => {
+	new PerfectScrollbar(element);
+});
+</script>
