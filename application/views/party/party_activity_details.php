49742ebaec29ca2d108db5e740163cf50176bdac
Jagdish Patel <jagdishpatelsoft@gmail.com>
2024-08-22 15:30:57 +0530

    dd

--- application/views/party/party_activity_details.php
+++ application/views/party/party_activity_details.php
@@ -8,7 +8,7 @@
             foreach($activityDetails as $row):
                 $btn = $link = '';
 
-                $dropDown = '<a class="dropdown-toggle lead-action" data-bs-toggle="dropdown" href="#" role="button">'.getIcon('more_v').'</a>
+                $dropDown = '<a class="dropdown-toggle lead-action float-end" data-bs-toggle="dropdown" href="#" role="button">'.getIcon('more_v').'</a>
 				<div class="dropdown-menu">'.$btn.'</div>';
                 
                 if(in_array($row->lead_stage,[4,6,7]))
@@ -18,23 +18,7 @@
                     if($row->lead_stage == 7){$linkUrl = base_url('salesOrder/printOrder/'.$row->ref_id);}
                     $link =' #<a href="'.$linkUrl.'" target="_blank"><span>'.$row->ref_no.'</span></a>';
                 }
-
-                /*
-                echo '<div class="activity-info">
-					<div class="icon-info-activity"><i class=""></i></div>
-                    <div class="activity-info-text">
-                        <div class="d-flex justify-content-between align-items-center">
-                            <h6 class="m-0 fs-13"></h6>
-                            
-                            <span class="text-muted w-30 d-block font-12">
-                            '.date("d F",strtotime($row->created_at)).$dropDown.'</span>
-                        </div>
-                        <p class=" m-1 font-12"><i class="fa fa-user"></i> '.$row->created_by_name.'</p>
-                        <p class="text-muted m-1 font-12">'.$row->remark.$link.'</p>
-                        
-                    </div>
-                </div>';
-                */
+                
                 echo '<div class="timeline-line">
                             <div class="item-timeline timeline-new">
                                 <div class="t-dot">
@@ -42,7 +26,7 @@
                                 </div>
                                 <div class="t-content">
                                     <div class="t-uppercontent">
-                                        <h5 class="font-bold">'.$row->notes.$link.$dropDown.'</h5>
+                                        <h5 class="font-bold w-100">'.$row->notes.$link.$dropDown.'</h5>
                                     </div>
                                     '.(!empty($row->remark) ? '<p class="text-dark">'.$row->remark.'</p>' : '').'
                                     <p>'.date("d F, y",strtotime($row->created_at)).'</p>
