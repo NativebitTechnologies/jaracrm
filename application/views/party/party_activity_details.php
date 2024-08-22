f49919b4cf5330a23a7d5f7ba5e8853500876a07
Jagdish Patel <jagdishpatelsoft@gmail.com>
2024-08-22 18:23:55 +0530

    asd

--- application/views/party/party_activity_details.php
+++ application/views/party/party_activity_details.php
@@ -6,24 +6,10 @@
         <?php
             echo '<div class="widget widget-activity-five no-box-shadow no-border">';
             foreach($activityDetails as $row):
-                $btn = $link = $icon = $iconColor = '';
-                $btn = '<a class="dropdown-item" href="javascript:void(0);" > Create User</a>
-                        <a class="dropdown-item" href="javascript:void(0);" > Sales Enquiry</a>
-                        <a class="dropdown-item" href="javascript:void(0);" > Sales Quotation</a>
-                        <a class="dropdown-item" href="javascript:void(0);" >Sales Order</a>';
+                $btns = $link = $icon = $iconColor = '';
                 
-                /*$dropDown = '<a class="dropdown-toggle lead-action float-end" data-bs-toggle="dropdown" href="#" role="button">'.getIcon('more_v').'</a>
-				<div class="dropdown-menu left" aria-labelledby="elementDrodpown1" style="will-change: transform;">'.$btn.'</div>';*/
-                $dropDown = '';
-                $bottomSection = '<div class="timeline-bottom">
-                                    <div class="tb-section-1">
-                                        <span data-taskdate="28 Apr">'.getIcon('calender').' 28 Apr </span>
-                                    </div>
-                                    <div class="tb-section-2">
-                                        <a class="" href="javascript:void(0);" >'.getIcon('edit').'</a>
-                                        <a class="danger" href="javascript:void(0);" >'.getIcon('delete').'</a>
-                                    </div>
-                                </div>';
+                $btns = '<a class="" href="javascript:void(0);" >'.getIcon('edit').'</a>
+                                   <a class="danger" href="javascript:void(0);" >'.getIcon('delete','','danger-svg').'</a>';
 
                 if(in_array($row->lead_stage,[4,6,7]))
                 {
@@ -48,10 +34,7 @@
                                         <div class="tb-section-1">
                                             <p>'.date("d F, y",strtotime($row->created_at)).'</p>
                                         </div>
-                                        <div class="tb-section-2">
-                                            <a class="" href="javascript:void(0);" >'.getIcon('edit').'</a>
-                                            <a class="danger" href="javascript:void(0);" >'.getIcon('delete','','danger-svg').'</a>
-                                        </div>
+                                        <div class="tb-section-2">'.$btns.'</div>
                                     </div>
                                 </div>
                             </div>';
